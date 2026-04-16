7c7f9df0182714bcb05413325f65303c872e8c6b
Muntaga Sallah <muntaga@hotmail.com>
2026-04-16 07:16:46 +0000

    auto create admin

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
 
-
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='muntaga@hotmail.com',
+                password='admin123'
+            )
