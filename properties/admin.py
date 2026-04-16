a07710c60f5f1ca62385b6ef62c6d707346692cc
Muntaga Sallah <muntaga@hotmail.com>
2026-04-16 17:09:40 +0000

    customize admin titles

--- properties/admin.py
+++ properties/admin.py
@@ -1,21 +1,5 @@
 from django.contrib import admin
-from .models import Apartment, Inquiry, Booking, ApartmentImage
 
-
-class ApartmentImageInline(admin.TabularInline):
-    model = ApartmentImage
-    extra = 3
-
-
-@admin.register(Apartment)
-class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'location', 'price', 'available')
-    inlines = [ApartmentImageInline]
-
-
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'apartment', 'check_in', 'check_out', 'status')
-
-
-admin.site.register(Inquiry)
+admin.site.site_header = "Muntaga's Property Hub Admin"
+admin.site.site_title = "Property Hub Admin"
+admin.site.index_title = "Administration"
