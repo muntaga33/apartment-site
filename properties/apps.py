5b3ac4afa17a3fd87517bf734eaf47c409f7633f
Muntaga Sallah <muntaga@hotmail.com>
2026-04-16 14:52:24 +0000

    auto create render admin

--- properties/apps.py
+++ properties/apps.py
@@ -1,15 +1,24 @@
 from django.apps import AppConfig
 
+
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
 
     def ready(self):
-        from django.contrib.auth.models import User
+        import os
+        if os.environ.get("RENDER") != "true":
+            return
 
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email='muntaga@hotmail.com',
-                password='admin123'
-            )
+        try:
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+
+            username = "admin"
+            email = "muntaga@hotmail.com"
+            password = "admin12345"
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username, email, password)
+        except Exception:
+            pass
