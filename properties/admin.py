1301086c2bfb9ad0c9104ae2bff4ef107e6e3422
Muntaga Sallah <muntaga@hotmail.com>
2026-04-17 23:19:49 +0000

    register models in admin

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from .models import Apartment, Booking, Inquiry
+
 
 admin.site.site_header = "Muntaga's Property Hub Admin"
 admin.site.site_title = "Property Hub Admin"
 admin.site.index_title = "Administration"
+
+
+# Register models so they appear in admin
+admin.site.register(Apartment)
+admin.site.register(Booking)
+admin.site.register(Inquiry)
