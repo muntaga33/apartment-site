7c5fe2baa15207c8d77ccaff2a001658820da40f
Muntaga Sallah <muntaga@hotmail.com>
2026-04-18 00:10:47 +0000

    fix admin registration fully

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Apartment, Booking, Inquiry
+from .models import Apartment, ApartmentImage, Booking, Inquiry
 
 
 admin.site.site_header = "Muntaga's Property Hub Admin"
@@ -7,7 +7,15 @@ admin.site.site_title = "Property Hub Admin"
 admin.site.index_title = "Administration"
 
 
-# Register models so they appear in admin
-admin.site.register(Apartment)
+class ApartmentImageInline(admin.TabularInline):
+    model = ApartmentImage
+    extra = 1
+
+
+class ApartmentAdmin(admin.ModelAdmin):
+    inlines = [ApartmentImageInline]
+
+
+admin.site.register(Apartment, ApartmentAdmin)
 admin.site.register(Booking)
 admin.site.register(Inquiry)
