a78d21ae30dd89ae29219ab51ec9d481aa55b13d
Muntaga Sallah <muntaga@hotmail.com>
2026-04-25 06:29:10 +0000

    simplify apartment admin

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Apartment, ApartmentImage, Booking, Inquiry
+from .models import Apartment, Booking, Inquiry
 
 
 admin.site.site_header = "Muntaga's Property Hub Admin"
@@ -7,15 +7,12 @@ admin.site.site_title = "Property Hub Admin"
 admin.site.index_title = "Administration"
 
 
-class ApartmentImageInline(admin.TabularInline):
-    model = ApartmentImage
-    extra = 1
-
-
+@admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
-    inlines = [ApartmentImageInline]
+    list_display = ("title", "location", "price", "available", "is_featured")
+    list_filter = ("available", "is_featured", "location")
+    search_fields = ("title", "location")
 
 
-admin.site.register(Apartment, ApartmentAdmin)
 admin.site.register(Booking)
 admin.site.register(Inquiry)
