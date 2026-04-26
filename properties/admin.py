806e14e5938a8e0a09d9ac16a0f3548d3531b6ad
Muntaga Sallah <muntaga@hotmail.com>
2026-04-26 20:39:07 +0000

    add apartment image gallery admin

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Apartment, Booking, Inquiry
+from .models import Apartment, ApartmentImage, Booking, Inquiry
 
 
 admin.site.site_header = "Muntaga's Property Hub Admin"
@@ -7,11 +7,22 @@ admin.site.site_title = "Property Hub Admin"
 admin.site.index_title = "Administration"
 
 
+class ApartmentImageInline(admin.TabularInline):
+    model = ApartmentImage
+    extra = 3
+
+
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
     list_display = ("title", "location", "price", "available", "is_featured")
     list_filter = ("available", "is_featured", "location")
     search_fields = ("title", "location")
+    inlines = [ApartmentImageInline]
+
+
+@admin.register(ApartmentImage)
+class ApartmentImageAdmin(admin.ModelAdmin):
+    list_display = ("apartment", "image")
 
 
 admin.site.register(Booking)
